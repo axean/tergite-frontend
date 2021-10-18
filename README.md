2a7ac6038c39a9bf9d6eda81dfdd416a7010d6f7
Miroslav Dobsicek <dobsicek@chalmers.se>
2021-10-18 15:22:01 +0200

    [BC-38] Use port number from .env file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Tergite MSS
+# Installation
 
-Main Service Server (MSS) SW pack. Dev2.
+## Prerequisites
+* Redis
+* (optional) Use Anaconda environment. With Python 3.8.
+
+## Package installation
+* cd tergite-mss
+* pip install -e .
+* create .env file with MSS configuration. See dot-env-template.txt.
+
+## Start
+* ./start_mss.sh
+
--- start_mss.sh
+++ start_mss.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+exit_with_error () {
+    echo "Port configuration failed. Use MSS_PORT=<num> in the .env file."
+    exit 1
+}
+
+# port handling
+PORT_CONFIG=$(grep MSS_PORT= .env)               # eg: MSS_PORT=5000
+PORT_NUMBER="${PORT_CONFIG#*=}"                  # extract the number
+[[ -z "$PORT_NUMBER" ]]  &&  exit_with_error     # validation
+[[ ! "$PORT_NUMBER" =~ ^[0-9]+$ ]]  &&  exit_with_error
 
 # rest-api
-uvicorn --host 0.0.0.0 --port 5000 rest_api:app --reload
+uvicorn --host 0.0.0.0 --port "$PORT_NUMBER" rest_api:app --reload
