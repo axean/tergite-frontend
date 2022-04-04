dee4ecaa129a3f068c0b20b5e0bd96f673a9cfb9
VladADragos <vlad6620@yahoo.com>
2022-04-04 21:53:12 +0200

    nvm linter is bricked

--- src/components/ConnectivityMap.tsx
+++ src/components/ConnectivityMap.tsx
@@ -119,7 +119,7 @@ const VisxChart: React.FC<VisxChartProps> = ({ data, height, width, backgroundCo
 			data.nodes.map(({ x, y }) => {
 				return { x: xScale(x / 2), y: yScale(1 - y / 2) - yMax / 2 };
 			}),
-		[data.nodes, xMax, yMax, xScale, yScale]
+		[data.nodes, xScale, yScale, xMax, yMax]
 	);
 	const newLinks = useMemo(
 		() =>
@@ -135,7 +135,7 @@ const VisxChart: React.FC<VisxChartProps> = ({ data, height, width, backgroundCo
 					}
 				};
 			}),
-		[data.links, xMax, yMax]
+		[data.links, xMax, yMax, xScale, yScale]
 	);
 
 	return (
