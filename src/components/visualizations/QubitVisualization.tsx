d827a93ff586e8ba49f4aa5e41e19e26f1a6b95b
VladADragos <vlad6620@yahoo.com>
2022-04-05 00:25:11 +0200

    made the links render on the correct positions

--- src/components/visualizations/QubitVisualization.tsx
+++ src/components/visualizations/QubitVisualization.tsx
@@ -18,23 +18,45 @@ const dataSample = {
 
 	links: [
 		{ source: nodes[0], target: nodes[1] },
-
+		{ source: nodes[0], target: nodes[2] },
 		{ source: nodes[1], target: nodes[3] },
-
-		{ source: nodes[2], target: nodes[0] },
-
 		{ source: nodes[3], target: nodes[2] }
 	]
 };
 
+function fixFlowDirection(links) {
+	// this function ensures that for all links {x0,y0,x1,y1} x0 < x1 and y0 < y1
+	// this is needed for the padding of the links to work correctly
+	console.log('before', links);
+	const newLinks = links.map((el) => {
+		let { source, target } = el;
+		let temp;
+		if (source.x < target.x) {
+			temp = source;
+			source = target;
+			target = temp;
+		} else if (source.y < target.y) {
+			temp = source;
+			source = target;
+			target = temp;
+		}
+
+		return { source, target, vertical: source.x === target.x ? true : false };
+	});
+
+	console.log('after', newLinks);
+	return newLinks;
+}
+const newData = { nodes: dataSample.nodes, links: fixFlowDirection(dataSample.links) };
+console.log('new link', newData.links);
 const QubitVisualization: React.FC<QubitVisualizationProps> = () => {
 	return (
 		<Flex gap='4'>
 			<Box flex='1'>
-				<ConnectivityMap data={dataSample} type='node' />
+				<ConnectivityMap data={newData} type='node' />
 			</Box>
 			<Box flex='1'>
-				<ConnectivityMap data={dataSample} type='node' />
+				<ConnectivityMap data={newData} type='link' />
 			</Box>
 		</Flex>
 	);
