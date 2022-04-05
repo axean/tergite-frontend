7e7b726ca17736354368ad48ef8daff2f01b49ea
hshanz <david00gbg@hotmail.com>
2022-04-05 16:35:54 +0200

    Datepicker done with popout

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,3 +1,4 @@
+import { Popover, PopoverTrigger, Button, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverHeader, PopoverBody } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { DateRange } from 'react-date-range';
 import sv from 'react-date-range/dist/locale/';
@@ -14,15 +15,31 @@ const DatePicker = () => {
 		}
 	]);
 
+	const getDates = () =>{
+		return date[0].startDate.toDateString().slice(4,10) + ' - ' + date[0].endDate.toDateString().slice(4,10)
+	}
+
+	console.log(getDates())
+
 	return (
-		<DateRange
-			editableDateInputs={true}
-			onChange={(item) => setDate([item.selection])}
-			moveRangeOnFirstSelection={false}
-			ranges={date}
-			locale={sv}
-			weekStartsOn={1}
-		/>
+		<Popover>
+  			<PopoverTrigger>
+   			 <Button boxShadow='4px 4px 2px 1px rgba(0, 0, 0, .1)' _focus={{outline: 'none'}}>Period: {getDates()}</Button>
+  			</PopoverTrigger>
+ 		 <PopoverContent _focus={{outline: 'none'}}>
+			<PopoverArrow />
+				<DateRange
+					editableDateInputs={true}
+					onChange={(item) => setDate([item.selection])}
+					moveRangeOnFirstSelection={false}
+					ranges={date}
+					locale={sv}
+					weekStartsOn={1}
+				/>
+			</PopoverContent>
+		</Popover>
+
+		
 	);
 };
 
