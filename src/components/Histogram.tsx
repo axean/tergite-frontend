0a79ffcd9f8078211f833ebe5534a7003e2cd59e
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2022-04-06 12:57:38 +0000

    finished styling

--- src/components/Histogram.tsx
+++ src/components/Histogram.tsx
@@ -1,6 +1,17 @@
+import { Flex } from '@chakra-ui/react';
+import { fill } from 'lodash';
 import React from 'react';
 import { useQuery } from 'react-query';
-import { VictoryAxis, VictoryChart, VictoryHistogram, VictoryLabel } from 'victory';
+import {
+	Box,
+	LineSegment,
+	VictoryAxis,
+	VictoryChart,
+	VictoryHistogram,
+	VictoryLabel,
+	VictoryTheme
+} from 'victory';
+import RadioButtons from './RadioButtons';
 
 interface HistogramProps {}
 
@@ -18,10 +29,27 @@ const Histogram: React.FC<HistogramProps> = ({}) => {
 	// const t1data = data.qubits.map(qubit => ({x: qubit.dynamic_properties[0].value}));
 
 	return (
-		<VictoryChart domainPadding={10}>
-			<VictoryLabel x={400} y={290} textAnchor={'middle'} text={'T1(us)'} />
+		<VictoryChart>
+			<VictoryLabel x={400} y={280} textAnchor={'middle'} text={'T1(us)'}
+			style= {[{fill: '#374151'}]} />
+			<VictoryAxis
+				tickCount={14}
+				style={{
+					axis: { stroke: '#9CA3AF', },
+					tickLabels: { fontSize: 12, padding: 5, fill: '#9CA3AF' }
+				}}
+			/>
+			<VictoryAxis
+				dependentAxis
+				style={{
+					axis: { stroke: 0 },
+					tickLabels: { fontSize: 12, padding: 5, fill: '#9CA3AF' },
+					grid: { stroke: '#9CA3AF', strokeWidth: 1, strokeDasharray:'8'}
+				}}
+			/>
+
 			<VictoryHistogram
-				cornerRadius={5}
+				cornerRadius={0}
 				binSpacing={3}
 				style={{
 					data: {
@@ -32,6 +60,8 @@ const Histogram: React.FC<HistogramProps> = ({}) => {
 				data={data.qubits.map((qubit) => ({
 					x: qubit.dynamic_properties[0].value * 1000000
 				}))}
+				//comment out hard coded bins to let d3 decide what the bins should be
+				bins={[30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]}
 			/>
 		</VictoryChart>
 	);
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,9 +1,10 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import Histogram from '../components/Histogram';
 
 interface testProps {}
 
 const test: React.FC<testProps> = ({}) => {
-	return <Histogram></Histogram>;
+	return	<Box bg='white'><Histogram></Histogram></Box>;
 };
 export default test;
