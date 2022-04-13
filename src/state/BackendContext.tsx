d338b6a4e81d1dfc4679b36e91d341e91c3196ec
VladADragos <vlad6620@yahoo.com>
2022-04-13 13:55:26 +0200

    added ability to set links and nodes in context

--- src/state/BackendContext.tsx
+++ src/state/BackendContext.tsx
@@ -1,8 +1,12 @@
 import React, { ReactNode, useState, useReducer } from 'react';
 
+type nodeType = 'qubits' | 'resonators';
+type linkType = 'couplers';
 export type BackendContextState = {
 	selectedNode: number;
 	selectedLink: number;
+	nodeType: nodeType;
+	linkType: linkType;
 	timeFrom: Date;
 	timeTo: Date;
 	nodes: Point[];
@@ -15,24 +19,50 @@ type BackendContextProps = [
 ];
 
 export enum MapActions {
-	SELECT_NODE = 0,
-	SELECT_LINK
+	SELECT_NODE = 'SELECT_NODE',
+	SELECT_LINK = 'SELECT_LINK',
+	SET_NODES = 'SET_NODES',
+	SET_LINKS = 'SET_LINKS',
+	SET_NODE_TYPE = 'SET_NODE_TYPE',
+	SET_LINK_TYPE = 'SET_LINK_TYPE'
 }
 
 export enum DateActions {
-	SET_TIME_FROM = 2,
-	SET_TIME_TO
+	SET_TIME_FROM = 'SET_TIME_FROM',
+	SET_TIME_TO = 'SET_TIME_TO'
 }
 
 function reducer(
 	state: BackendContextState,
-	action: { type: DateActions; payload: Date } | { type: MapActions; payload: number }
+	action:
+		| { type: DateActions; payload: Date }
+		| { type: MapActions; payload: number | Link[] | Point[] | nodeType | linkType }
 ): BackendContextState {
 	switch (action.type) {
 		case MapActions.SELECT_NODE:
-			return { ...state, selectedNode: action.payload };
+			return { ...state, selectedNode: action.payload as number };
 		case MapActions.SELECT_LINK:
-			return { ...state, selectedLink: action.payload };
+			return { ...state, selectedLink: action.payload as number };
+		case MapActions.SET_NODES:
+			return { ...state, nodes: action.payload as Point[] };
+		case MapActions.SET_LINKS:
+			return {
+				...state,
+				links: action.payload.map((e) => {
+					const from = state.nodes.find((n) => n.id == e.from);
+					const to = state.nodes.find((n) => n.id == e.to);
+					return {
+						...e,
+						from: { id: from.id, x: from.x, y: from.y },
+						to: { id: to.id, x: to.x, y: to.y }
+					};
+				}) as Link[]
+			};
+		case MapActions.SET_NODE_TYPE:
+			return { ...state, nodeType: action.payload as nodeType };
+		case MapActions.SET_LINK_TYPE:
+			return { ...state, linkType: action.payload as linkType };
+
 		case DateActions.SET_TIME_FROM:
 			return { ...state, timeFrom: action.payload };
 		case DateActions.SET_TIME_TO:
@@ -50,6 +80,8 @@ const BackendContextProvider: React.FC<BackendContextProviderProps> = ({ childre
 	const data: BackendContextState = {
 		selectedNode: -1,
 		selectedLink: -1,
+		nodeType: 'qubits',
+		linkType: 'couplers',
 		timeFrom,
 		timeTo: new Date(),
 		nodes: [],
