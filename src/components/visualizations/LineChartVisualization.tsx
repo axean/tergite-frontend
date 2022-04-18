eec4f5c9c79029c354d6b775be742ba4140c1c00
jyborn <jyborn@student.chalmers.se>
2022-04-18 13:54:42 +0200

    real data from api kind of working

--- src/components/visualizations/LineChartVisualization.tsx
+++ src/components/visualizations/LineChartVisualization.tsx
@@ -1,19 +1,23 @@
 import { Box, Flex, FormLabel, Select } from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 import { ParentSize } from '@visx/responsive';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LineChart from '../lineChart/LineChart';
 import RadioButtons from '../RadioButtons';
+
+interface LineData {
+	x: number;
+	y: number;
+}
 
 const LineChartVisualization = ({ backend }) => {
 	const domainQuery = useQuery('linechartDomain', () =>
 		fetch('http://qtl-webgui-2.mc2.chalmers.se:8080/devices/' + backend + '/type4_domain')
 			.then((res) => res.json())
 			.then((json) => {
-				console.log(json);
+				setDomain(json);
 				setDomTabs(Object.keys(json));
 				setTabDom(Object.keys(json)[0]);
-				setDomain(json);
 			})
 	);
 
@@ -21,19 +25,42 @@ const LineChartVisualization = ({ backend }) => {
 		fetch('http://qtl-webgui-2.mc2.chalmers.se:8080/devices/' + backend + '/type4_codomain')
 			.then((res) => res.json())
 			.then((json) => {
+				setCoDomain(json);
 				setCoDomTabs(Object.keys(json));
 				setTabCo(Object.keys(json)[0]);
-				setCoDomain(json);
 			})
 	);
 
 	const [domTabs, setDomTabs] = useState<string[]>();
 	const [coDomTabs, setCoDomTabs] = useState<string[]>();
-	const [domain, setDomain] = useState<string[]>();
-	const [coDomain, setCoDomain] = useState<string[]>();
 	const [tabDom, setTabDom] = useState<string>();
 	const [tabCo, setTabCo] = useState<string>();
-	const [lineData, setLineData] = useState<{ x: number; y: number }[]>();
+	const [paramDom, setParamDom] = useState<string>();
+	const [paramCo, setParamCo] = useState<string>();
+	const [domain, setDomain] = useState<string[]>();
+	const [coDomain, setCoDomain] = useState<string[]>();
+	const [lineData, setLineData] = useState<LineData[]>([
+		{ x: 0, y: 0 },
+		{ x: 10, y: 12 },
+		{ x: 30, y: 50 },
+		{ x: 35, y: 55 },
+		{ x: 100, y: 15 }
+	]);
+
+	useEffect(() => {
+		if (domain && coDomain && paramDom && paramCo) {
+			console.log(domain);
+			console.log(coDomain);
+			let data = [];
+			for (let i = 0; i < 5; i++) {
+				data.push({
+					x: domain[tabDom][i][paramDom][0].value,
+					y: coDomain[tabCo][i][paramCo][0].value
+				});
+			}
+			setLineData(data);
+		}
+	}, [paramDom, paramCo]);
 
 	const getParameters = (options: string[], selectedTab: string) => {
 		let parameters = [];
@@ -49,8 +76,6 @@ const LineChartVisualization = ({ backend }) => {
 		return parameters;
 	};
 
-	const getLineData = () => {};
-
 	if (domainQuery.isLoading || coDomainQuery.isLoading) return <span>Loading...</span>;
 
 	if (domainQuery.error || coDomainQuery.error) return <span>Error</span>;
@@ -63,28 +88,28 @@ const LineChartVisualization = ({ backend }) => {
 				<Flex pb={'16px'} alignItems='center'>
 					<FormLabel fontSize={'large'}> Domain: </FormLabel>
 					<RadioButtons tabs={domTabs} setTab={setTabDom} />
-					<Select>{getParameters(domain, tabDom)}</Select>
+					<Select
+						onChange={(e) => {
+							setParamDom(e.target.value);
+						}}
+					>
+						{getParameters(domain, tabDom)}
+					</Select>
 				</Flex>
 				<Flex alignItems='center'>
 					<FormLabel fontSize={'large'}> Codomain: </FormLabel>
 					<RadioButtons tabs={coDomTabs} setTab={setTabCo} />
-					<Select>{getParameters(coDomain, tabCo)}</Select>
+					<Select
+						onChange={(e) => {
+							setParamCo(e.target.value);
+						}}
+					>
+						{getParameters(coDomain, tabCo)}
+					</Select>
 				</Flex>
 			</Box>
 			<ParentSize>
-				{({ width, height }) => (
-					<LineChart
-						data={[
-							{ x: 0, y: 0 },
-							{ x: 10, y: 12 },
-							{ x: 30, y: 50 },
-							{ x: 35, y: 55 },
-							{ x: 100, y: 15 }
-						]}
-						width={width}
-						height={height}
-					/>
-				)}
+				{({ width, height }) => <LineChart data={lineData} width={width} height={height} />}
 			</ParentSize>
 		</>
 	);
