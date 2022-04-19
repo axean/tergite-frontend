4b38c2292928290252900158aaf55c569e6d2a29
VladADragos <vlad6620@yahoo.com>
2022-04-19 14:55:00 +0200

    fixed the styling and added sample name

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -98,7 +98,7 @@ export default Detail;
 
 function SidePanel({ isCollapsed, setCollapsed, MdFirstPage, backend, type }) {
 	const showMap = type !== undefined && type !== 'Qubitmap';
-	const { isLoading, data, error } = useQuery('backendDetail', () =>
+	const { isLoading, data, error } = useQuery<API.Response.DeviceDetail>('backendDetail', () =>
 		fetch('http://qtl-webgui-2.mc2.chalmers.se:8080/devices/' + backend).then((res) =>
 			res.json()
 		)
@@ -127,22 +127,98 @@ function SidePanel({ isCollapsed, setCollapsed, MdFirstPage, backend, type }) {
 						</Box>
 					</Flex>
 				) : (
-					<>
+					<Box pl='2'>
 						<Flex px={0} justifyContent='space-between'>
-							<CardBackend {...data} />
+							<BackendInfo {...data} />
+
 							<Button ml='5' p='1' onClick={() => setCollapsed(!isCollapsed)}>
 								<Icon as={MdFirstPage} w={8} h={8} />
 							</Button>
 						</Flex>
-						<Text fontSize='3xl' color='black' mt='10'>
+						<Text fontSize='2xl' fontWeight='bold' color='black' mt='10'>
 							Description
 						</Text>
 						<Text fontSize='lg' color='black'>
 							{data.description}
 						</Text>
-					</>
+					</Box>
 				)}
 			</Flex>
 		)
 	);
 }
+
+type BackendInfoProps = Omit<
+	API.Response.DeviceDetail,
+	'resonators' | 'couplers' | 'qubits' | 'gates'
+>;
+
+const BackendInfo: React.FC<BackendInfoProps> = ({
+	backend_name,
+	backend_version,
+	n_qubits,
+	is_online,
+	last_update_date,
+	online_date,
+	sample_name
+}) => {
+	return (
+		<Box
+			bg={is_online ? 'white' : 'gray.100'}
+			borderRadius='md'
+			color={is_online ? 'black' : 'gray.500'}
+			flex='1'
+		>
+			<Flex justify='space-between'>
+				{' '}
+				<Text fontSize='2xl' fontWeight='extrabold'>
+					{' '}
+					{backend_name}
+				</Text>{' '}
+				<Flex align='center'>
+					<Text fontSize='sm' mr='2'>
+						{is_online ? 'online' : 'offline'}{' '}
+					</Text>
+					<Box
+						display='inline-block'
+						w='4'
+						h='4'
+						bg={is_online ? 'green.400' : 'red.400'}
+						borderRadius='full'
+					></Box>
+				</Flex>
+			</Flex>
+			<Flex mt='2'>
+				<Text fontSize='md' fontWeight='regular' mr='2'>
+					version:
+				</Text>
+				<Text fontWeight='bold'>{backend_version}</Text>
+			</Flex>
+			<Flex>
+				<Text fontSize='md' fontWeight='regular' mr='2'>
+					qubits:
+				</Text>
+				<Text fontWeight='bold'>{n_qubits}</Text>
+			</Flex>
+			<Flex>
+				<Text fontSize='md' fontWeight='regular' mr='2'>
+					last update:
+				</Text>
+				<Text fontWeight='bold'>{last_update_date?.split('T')[0]}</Text>
+			</Flex>
+			<Flex>
+				<Text fontSize='md' fontWeight='regular' mr='2'>
+					sample name:
+				</Text>
+				<Text fontWeight='bold'>{sample_name}</Text>
+			</Flex>
+			<Text fontWeight='bold' fontSize='sm' mt='2' color={is_online ? 'gray.700' : 'inherit'}>
+				{' '}
+				{is_online ? `Online since` : `Offline since`}
+			</Text>
+			<Text fontWeight='bold'>
+				{is_online ? online_date.split('T')[0] : last_update_date.split('T')[0]}
+			</Text>
+		</Box>
+	);
+};
