79871edf775552c5f93e1d1e5ee78a1f92ed1b19
hshanz <david00gbg@hotmail.com>
2022-05-11 14:11:50 +0200

    Done maybe?

--- cypress/integration/tables.spec.js
+++ cypress/integration/tables.spec.js
@@ -1,7 +1,39 @@
 describe('table page', () => {
 	it('load tables', () => {
         cy.visit('/Pingu?type=Tableview')
-        cy.wait(100)
-        cy.get('data-cy-qubit-table')
+        cy.wait(200)
+        cy.get(['data-cy-qubit-table']).should('exist')
+    })
+
+    it('Load qubit table',() =>{
+        cy.visit('/Pingu?type=Tableview')
+        cy.wait(200)
+        cy.get(['data-index=0']).click()
+        cy.wait(200)
+        cy.get(['data-cy-qubit-table']).should('exist')
+    })
+
+    it('Load gate table',() =>{
+        cy.visit('/Pingu?type=Tableview')
+        cy.wait(200)
+        cy.get(['data-index=1']).click()
+        cy.wait(200)
+        cy.get(['data-cy-gate-table']).should('exist')
+    })
+
+    it('Load coupler table',() =>{
+        cy.visit('/Pingu?type=Tableview')
+        cy.wait(200)
+        cy.get(['data-index=2']).click()
+        cy.wait(200)
+        cy.get(['data-cy-coupler-table']).should('exist')
+    })
+
+    it('Load coupler table',() =>{
+        cy.visit('/Pingu?type=Tableview')
+        cy.wait(200)
+        cy.get(['data-index=3']).click()
+        cy.wait(200)
+        cy.get(['data-cy-res-table']).should('exist')
     })
 });
