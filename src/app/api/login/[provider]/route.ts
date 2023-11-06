4477604e0033e8124664a45998aed8689e60b88a
Martin Ahindura <ahindura@chalmers.se>
2023-11-06 10:01:32 +0100

    Fix 'UND_ERR_CONNECT_TIMEOUT' error when logging in

--- src/app/api/login/[provider]/route.ts
+++ src/app/api/login/[provider]/route.ts
@@ -1,5 +1,6 @@
 import { API } from '@/types';
 import { NextResponse } from 'next/server';
+import axios from 'axios';
 
 // ensure this is not reduced to compile-time variables
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
@@ -10,20 +11,17 @@ export async function GET(request: Request, { params }: { params: { provider: st
 	const host = request.headers.get('host');
 	const redirectUrl = process.env.OAUTH_REDIRECT_URI || `http://${host}`;
 	const nextUrl = encodeURI(redirectUrl);
-	// issue with fetch and localhost:
-	// https://github.com/vercel/next.js/issues/45315#issuecomment-1406579321
-	const baseUrl = process.env.API_BASE_URL?.replace('localhost', '127.0.0.1');
+	const baseUrl = process.env.API_BASE_URL;
 	const authorizeUrl = `${baseUrl}/auth/app/${provider}/authorize?next=${nextUrl}`;
 
 	try {
-		const response = await fetch(authorizeUrl, { cache: 'no-store' });
-		if (response.ok) {
-			const body: API.Response.Authorize = await response.json();
-			return NextResponse.redirect(body.authorization_url);
+		const response = await axios.get(authorizeUrl);
+		if (response.status === 200) {
+			return NextResponse.redirect(response.data.authorization_url);
 		}
 		return response;
 	} catch (error) {
-		console.error(error);
+		console.error({ error, url: authorizeUrl });
 		return NextResponse.json({ detail: 'unexpected error' });
 	}
 }
