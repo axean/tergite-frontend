3e8971b0e762f4ccb283ded3713cdd2f0d02505e
Martin Ahindura <ahindura@chalmers.se>
2023-11-13 12:22:54 +0100

    Add test for automatic redirection to next url on login screen

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -34,20 +34,44 @@ users.forEach((user) => {
 						secure: false,
 						sameSite: 'lax'
 					});
-
-					cy.visit(`http://localhost:3000/login${queryStr}`);
 				});
 
 				it(`renders ${provider} login link`, () => {
+					cy.visit(`http://localhost:3000/login${queryStr}`);
+
 					cy.get(`[data-cy-login-with=${provider}]`)
 						.should('contain.text', `Login with ${provider}`)
 						.should('have.attr', 'href', `/api/login/${provider}${queryStr}`);
 				});
 
 				it(`click ${provider} login logs one in`, () => {
+					cy.visit(`http://localhost:3000/login${queryStr}`);
+
 					const nextUrl = nextQueryParam || `${process.env.OAUTH_REDIRECT_URI}/`;
 					const next = encodeURI(nextUrl);
 					cy.get(`[data-cy-login-with=${provider}]`).realClick();
+
+					cy.url().should('eq', `${next}`);
+					cy.get('[data-cy-nav-btn]').should('contain.text', 'Logout');
+				});
+
+				it(`automatically redirects to next page if already logged in`, () => {
+					cy.wrap(utils.generateJwt(user)).then((jwtToken) => {
+						const cookieName = process.env.COOKIE_NAME as string;
+						const domain = process.env.COOKIE_DOMAIN as string;
+
+						cy.setCookie(cookieName, jwtToken as string, {
+							domain,
+							httpOnly: true,
+							secure: false,
+							sameSite: 'lax'
+						});
+					});
+
+					cy.visit(`http://localhost:3000/login${queryStr}`);
+
+					const nextUrl = nextQueryParam || `http://localhost:3000/login`;
+					const next = encodeURI(nextUrl);
 
 					cy.url().should('eq', `${next}`);
 					cy.get('[data-cy-nav-btn]').should('contain.text', 'Logout');
