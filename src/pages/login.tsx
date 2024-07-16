ab51ea1c60f439f4e33aeef39f2b9ab9613cc051
Pontus Wikståhl <pontus.wikstahl@gmail.com>
2024-07-16 15:26:44 +0200

    MVP Login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,76 @@
-export default function LoginPage() {
-  /// FIXME: if logged in, redirect to home
-  return <div>Login</div>;
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+/** Logo Component */
+function Logo() {
+  return (
+    <div className="flex items-center justify-center text-center mb-8">
+      <div className="group shrink-0">
+        <p className="text-2xl font-semibold">WACQT</p>
+        <p className="font-thin text-base">
+          Wallenberg Centre for Quantum Technology
+        </p>
+      </div>
+    </div>
+  );
+}
+
+/** LoginForm Component */
+export default function LoginForm() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Logo /> {/* Logo placed above the Card */}
+      <Card className="max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link to="#" className="ml-auto inline-block text-sm underline">
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with SSO
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don't have an account?{" "}
+            <Link to="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
