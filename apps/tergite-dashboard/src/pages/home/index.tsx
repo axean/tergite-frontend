f0394ab36bd2bffeefa7015d8fec760590270e7f
Martin Ahindura <ahindura@chalmers.se>
2024-08-21 11:58:03 +0200

    Fix spacing on dashboard

--- apps/tergite-dashboard/src/pages/home/index.tsx
+++ apps/tergite-dashboard/src/pages/home/index.tsx
@@ -34,7 +34,7 @@ export function Home() {
 
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-3">
+      <div className="grid auto-rows-max items-start gap-4 lg:col-span-3">
         <div className="grid gap-4 sm:grid-cols-[250px_auto_auto]">
           <Card className="grid-fit-content">
             <CardHeader className="pb-4">
@@ -63,9 +63,6 @@ export function Home() {
               <DevicesTable data={devices.slice(0, 3)} />
             </CardContent>
           </Card>
-        </div>
-        <div className="flex items-center">
-          <div className="ml-auto flex items-center gap-2"></div>
         </div>
         <Card>
           <CardHeader className="px-7">
